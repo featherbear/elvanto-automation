083e5d884a074978e58bc9e965ce09fa904fbfbb
Andrew Wong <andrew12345@live.com>
2018-02-08 18:51:43 +1100

    Move exception into module class and implement validation call

--- modules/__stub__.py
+++ modules/__stub__.py
@@ -1,9 +1,9 @@
 from ElvantoAPIExtensions import ElvantoAPI
 
-class ModuleException(Exception):
-    pass
-
 class ModuleStub():
+    class ModuleException(Exception):
+        pass
+
     __VERSION__ = ""
     __NAME__ = ""
 
@@ -22,17 +22,25 @@ class ModuleStub():
                     _config.set(section, key, self.settings[section][key])
             with open(_configFilePath, "w") as _configFile:
                 _config.write(_configFile)
-            raise ModuleException("Created '%s'. Please enter in your API key to continue. Exiting" % _configFilePath)
+            raise self.ModuleException("Created '%s'. Please enter in your API key to continue. Exiting" % _configFilePath)
         elif not os.path.isfile(_configFilePath):
-            raise ModuleException(_configFilePath + " is not a file! Aborting")
+            raise self.ModuleException(_configFilePath + " is not a file! Aborting")
         _config.read(_configFilePath)
 
         for section in self.settings:
             for key in self.settings[section]:
                 val = _config.get(section, key)
-                if not val: raise ModuleException("Invalid setting for " + section + "." + key)
+                if not val: raise self.ModuleException("Empty setting for " + section + "." + key)
                 self.settings[section][key] = val
+        self.validate()
         self.conn = connectionObj
+
+
+    def validate(self):
+        # stub
+        return
+        raise self.ModuleException("Error")
+
 
     def run(self):
         pass
